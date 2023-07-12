a49ae104b7d101865ae9740e5c7c8456592969ca
Olena Hrechykhina <117777710+Elena-Hrechyk@users.noreply.github.com>
2023-07-12 09:28:14 -0700

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -36,8 +36,7 @@ const contactsSlice = createSlice({
     [deleteContact.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
-      const index = state.items.findIndex(item => item.id === action.payload);
-      state.items.splice(index, 1);
+      state.items.filter(item => item.id !== action.payload);
     },
     [deleteContact.rejected]: handleRejected,
   },
